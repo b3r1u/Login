13c080b368d9b1d76132e7bce2f129290d77bc92
Roberio Albuquerque <roberioalbuquerque.neto@gmail.com>
2024-05-07 11:36:43 -0300

    feat: Functionality to hi de login box for singUp

--- Login.js
+++ Login.js
@@ -1,17 +1,19 @@
-//Box de inputs
+//Box inputs
 
 const boxIn = document.querySelector('.box');
 const boxUp = document.querySelector('.box_add');
 
-//Botões das box 
+//Buttons Box's
 
 const buttonIn = document.querySelector('#in');
 const buttonAdd = document.querySelector('#add')
 const buttonUp = document.querySelector('#up')
 
+//Hiding the Box's
 
 buttonAdd.addEventListener('click', function() {
     boxIn.style.display = 'none';
     buttonAdd.style.display = 'none';
     boxUp.style.display = 'block'
 })
+
