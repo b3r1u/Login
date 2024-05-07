5872719d137f9d2e63d399d310a6524f543fb67f
Roberio Albuquerque <roberioalbuquerque.neto@gmail.com>
2024-05-07 11:18:49 -0300

    wip: created login behind de box

--- Login.js
+++ Login.js
@@ -1,1 +1,17 @@
-console.log('Git Flow!');
+//Box de inputs
+
+const boxIn = document.querySelector('.box');
+const boxUp = document.querySelector('.box_add');
+
+//Botões das box 
+
+const buttonIn = document.querySelector('#in');
+const buttonAdd = document.querySelector('#add')
+const buttonUp = document.querySelector('#up')
+
+
+buttonAdd.addEventListener('click', function() {
+    boxIn.style.display = 'none';
+    buttonAdd.style.display = 'none';
+    boxUp.style.display = 'block'
+})
